2eb55ebfcd17b605d1771fa3cb0c0d4e661dbc3e
swemi01 <38538914+swemi01@users.noreply.github.com>
2018-04-22 20:44:14 +0200

    qqq

--- sandbox/src/main/java/pl/stqa/pft/sandbox/my_first.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/my_first.java
@@ -1,8 +1,7 @@
 package pl.stqa.pft.sandbox;
 
-public class my_first
-{
-public static void main(String[] args) {
-System.out.println("Hello, word");
-}
+public class my_first {
+    public static void main(String[] args) {
+        System.out.println("Hello, word");
+    }
 }
