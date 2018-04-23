25ea564e73a242b03315f15d6dd5e095a171ef36
swemi01 <38538914+swemi01@users.noreply.github.com>
2018-04-23 20:34:59 +0200

    test

--- sandbox/src/main/java/pl/stqa/pft/sandbox/my_first.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/my_first.java
@@ -3,5 +3,6 @@ package pl.stqa.pft.sandbox;
 public class my_first {
     public static void main(String[] args) {
         System.out.println("Hello, word");
+        System.out.println(2+2);
     }
 }
