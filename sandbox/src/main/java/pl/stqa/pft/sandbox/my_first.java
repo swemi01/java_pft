9137495500b393d1866235586dabe4eea672f81f
swemi01 <38538914+swemi01@users.noreply.github.com>
2018-04-23 21:19:47 +0200

    funkcje

--- sandbox/src/main/java/pl/stqa/pft/sandbox/my_first.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/my_first.java
@@ -2,7 +2,31 @@ package pl.stqa.pft.sandbox;
 
 public class my_first {
     public static void main(String[] args) {
-        System.out.println("Hello, word");
-        System.out.println(2+2);
+
+        hello("word");
+        hello("szymon");
+
+        double l = 5;
+        System.out.println("powierzchnia boku kwadratu " + l + " = " + area(l));
+
+        double a =4;
+        double b = 6;
+        System.out.println("powierzchnia boku prostokąta " + a + " i " + b + area(a, b) );
+    }
+
+    public static void hello(String sombody)
+    {
+
+        System.out.println("Hello " + sombody);
+    }
+
+    public static double area (double len)
+    {
+        return len * len;
+    }
+
+    public static double area (double a, double b)
+    {
+        return a * b;
     }
 }
