ff3fe4942330789631e0156ceedd31f5c48f7f54
swemi01 <38538914+swemi01@users.noreply.github.com>
2018-04-26 22:10:15 +0200

    Point fail

---
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/Point.java
@@ -0,0 +1,21 @@
+package pl.stqa.pft.sandbox;
+
+public class Point
+{
+    public double x;
+    public double y;
+
+    public Point(double p1, double p2)
+    {
+        this.x = p1;
+        this.y = p2;
+    }
+
+    public static double distance(Point p1, Point p2)
+    {
+        Point px = new Point(4, 6);
+        Point py = new Point(7, 8);
+        
+        return Math.sqrt();
+    }
+}
