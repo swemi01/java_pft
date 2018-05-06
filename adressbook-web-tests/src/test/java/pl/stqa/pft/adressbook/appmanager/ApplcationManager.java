772adc733036a4fbd7d6fc773492cee14b1b4a7d
swemi01 <38538914+swemi01@users.noreply.github.com>
2018-05-06 15:33:30 +0200

    contact create function

--- adressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/appmanager/ApplcationManager.java
+++ adressbook-web-tests/src/test/java/pl/stqa/pft/adressbook/appmanager/ApplcationManager.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class ApplcationManager {
     FirefoxDriver wd;
 
+    private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
 
@@ -29,18 +30,11 @@ public class ApplcationManager {
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
-        login("admin", "secret");
+        sessionHelper = new SessionHelper(wd);
+        sessionHelper.login("admin", "secret");
     }
 
-    private void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
+
 
     public void stop() {
         wd.quit();
